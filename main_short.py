9622b75c477228f8289afc4ccf3bfdcf3c0dc7fb
DefCon-007 <ayushgoyal.iitkgp@gmail.com>
2016-09-17 07:26:19 +0000

    Modified main_shorts

--- main_short.py
+++ main_short.py
@@ -5,20 +5,21 @@ import selenium.common.exceptions
 import time
 from selenium.webdriver.common.proxy import *
 def main(file_path):
-	driver = webdriver.Chrome()
+	driver = webdriver.PhantomJS()
 	driver.get('http://expirebox.com/')
 	file_upload = driver.find_element_by_id('fileupload')  #finding the file upload element
-	#print ("Uploading file please wait")
+	print ("Uploading file please wait")
 	file_upload.send_keys(file_path)  #uploading file
 	while True :
 		try :
 			del_button = driver.find_element_by_xpath("//button[@class='btn btn-danger btndel']")
 			del_button.click()
-		   # print ("Upload Complete")
+			print ("Upload Complete")
 			break
 		except selenium.common.exceptions.ElementNotVisibleException :
 			time.sleep(1)
 			pass
+	time.sleep(5)
 	driver.switch_to_window(driver.window_handles[1])  #switching to the new tab which have links
 	del_link = driver.current_url + "?proceed=1" #getting delete file link
 	# getting download link
@@ -27,12 +28,16 @@ def main(file_path):
    # file_name = input("Enter a link name for : tinyurl.com/abc (Leave blank to use default filename) : ")
   #if file_name is "" : 
 	file_name = file_path.split('/')[-1][0:5]
+	original_file_name = file_name
 	count = 1
-	links = {"down": "X" , "del" : "X"}
-	while links['down']:
+	links = {"down": "" , "del" : ""}
+	flag = 1
+	while flag:
 		links['down'] = tinyurl.shorten(down_link , "SB-"+file_name)
 		links['del'] = tinyurl.shorten(del_link , "del-"+file_name)
-		file_name = file_name + str(count)
+		if links['down'] != 1:
+			flag = 0 
+		file_name = original_file_name + str(count)
 		count = count + 1 
 	driver.quit()
 	print (links)
