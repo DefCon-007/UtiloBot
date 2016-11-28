668134c6616810bf2db91259594525a910a13557
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-11-28 18:18:22 +0530

    Fixed indentation error

--- url_shortener.py
+++ url_shortener.py
@@ -5,14 +5,14 @@ logger = Logger.Logger(name='My_Logger')
 def goo_shorten_url(url):
 	with open('./GOOGLE_API_KEY', 'r') as f:
 		google_api = f.readline().rstrip('\n')
-    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(google_api)
-    payload = {'longUrl': url}
-    headers = {'content-type': 'application/json'}
-    r = requests.post(post_url, data=json.dumps(payload), headers=headers).json()
-    if "error" in r :
-    	logger.addLog(r)
-    	return "Error"
-    else : 
-    	return r['id']
+	post_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(google_api)
+	payload = {'longUrl': url}
+	headers = {'content-type': 'application/json'}
+	r = requests.post(post_url, data=json.dumps(payload), headers=headers).json()
+	if "error" in r :
+		logger.addLog(r)
+		return "Error"
+	else : 
+		return r['id']
 
 # goo_shorten_url("https://www.google.com")
